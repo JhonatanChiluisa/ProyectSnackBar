f0b9cbf6544c1d0a71cc2bea531ac8965e148e2e
@mlcruz2 <mlcruz2@espe.edu.ec>
2022-06-15 15:21:08 -0500

    primero

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <head>
 
-	<title>Bar - ESPEL</title>
+	<title>BAR - ESPEL</title>
 	<!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 11]>
